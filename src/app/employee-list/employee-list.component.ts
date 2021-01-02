7736b4615ac7d596d850e6a261c28405d4251412
krishna saparia <krishnasaparia43045@gmail.com>
2021-01-02 17:09:37 -0500

    Setup HttpRequest and constructed get req. and used observer from rxjs library.

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Employee} from '../employee';
+import {EmployeeService} from '../employee.service';
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
@@ -9,21 +10,27 @@ export class EmployeeListComponent implements OnInit {
 
   employees: Employee[];
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
-    this.employees = [{
-      id: 1,
-      firstName: 'krishna',
-      lastName: 'Saparia',
-      emailID: 'k12@gmail.com'
-    },
-      {
-        id: 2,
-        firstName: 'ishna',
-        lastName: 'paria',
-        emailID: 'k2@gmail.com'
-      }];
+    // this.employees = [{
+    //   id: 1,
+    //   firstName: 'krishna',
+    //   lastName: 'Saparia',
+    //   emailID: 'k12@gmail.com'
+    // },
+    //   {
+    //     id: 2,
+    //     firstName: 'ishna',
+    //     lastName: 'paria',
+    //     emailID: 'k2@gmail.com'
+    //   }];
+    this.getEmployees();
   }
-
+  // tslint:disable-next-line:typedef
+  private getEmployees(){
+    this.employeeService.getEmployeeList().subscribe(data => {
+      this.employees = data;
+    });
+  }
 }
