96b42dcd8c5789e4e386b0c5f23be8b5946c7153
krishna saparia <krishnasaparia43045@gmail.com>
2021-01-04 15:32:10 -0500

    Add Activated Router that contains info along with id to update details.

--- src/app/update-employee/update-employee.component.ts
+++ src/app/update-employee/update-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {EmployeeService} from '../employee.service';
+import {Employee} from '../employee';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-update-employee',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-employee.component.css']
 })
 export class UpdateEmployeeComponent implements OnInit {
-
-  constructor() { }
+  id: number;
+  employee: Employee = new Employee();
+  constructor(private employeeService: EmployeeService , private activeRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id = this.activeRoute.snapshot.params[`id`];
+    this.employeeService.getEmployeeById(this.id).subscribe(data => {
+      this.employee = data;
+    },
+      error => console.log(error));
   }
 
 }
