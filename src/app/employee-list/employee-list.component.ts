11a666023595ca2ce9e5f3dd15de5d17f35e5bf7
krishna saparia <krishnasaparia43045@gmail.com>
2021-01-04 18:30:50 -0500

    Add Changes In Delete Event.

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -27,10 +27,11 @@ export class EmployeeListComponent implements OnInit {
   updateEmployee(id: number){
     this.router.navigate(['update-employee', id]);
   }
+  // tslint:disable-next-line:typedef
   deleteEmployee(id: number){
     this.employeeService.deleteEmployee(id).subscribe( data => {
       console.log(data);
       this.getEmployees();
-    })
+    });
   }
 }
