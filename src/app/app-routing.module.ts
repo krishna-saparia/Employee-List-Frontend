d299322a810afd1080b945112a7c92e833a7c918
krishna saparia <krishnasaparia43045@gmail.com>
2021-01-04 15:14:30 -0500

    Create update button and add router to navigate update-employee.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import {EmployeeListComponent} from './employee-list/employee-list.component';
 import {CreateEmployeeComponent} from './create-employee/create-employee.component';
+import {UpdateEmployeeComponent} from './update-employee/update-employee.component';
 
 const routes: Routes = [
   { path: 'employees', component: EmployeeListComponent },
   { path: 'create-employee', component: CreateEmployeeComponent},
-  { path: '', redirectTo: 'employees', pathMatch: 'full'}
+  { path: '', redirectTo: 'employees', pathMatch: 'full'},
+  { path: 'update-employee/:id' , component: UpdateEmployeeComponent}
 ];
 
 
